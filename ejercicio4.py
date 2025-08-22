7f326fc58bc31ed9ae354a2159bdd5dc922fd5f6
David-Espinoza2007 <a01564484@tec.mx>
2025-08-22 23:28:01 +0000

    r

---
+++ ejercicio4.py
@@ -0,0 +1,13 @@
+print("Ingresa un número de champions: ")
+
+champs=int(input("dame un número de champions: "))
+
+match champs:
+    case 15:
+        print("Solo en la grandeza RM")
+    case 7:
+        print("Milán segundón")
+    case 6:
+        print("liverpool y bayern, no son mediocres")
+    case 5:
+        print("mejor nada")
